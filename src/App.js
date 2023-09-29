c82a3dca881b34c982c80469a6ea0a359ae713d3
Asheesh Banga <asheesh.banga@Asheeshs-Mac.local>
2023-09-29 17:17:28 -0400

    Pulling random user on button click now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const fetchRandomData = (pageNumber) => {
+  return axios
+    .get(`https://randomuser.me/api?page=${pageNumber}`)
+    .then(({data}) => {
+      console.log(data);
+      return data;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const getFullUserName = (userInfo) => {
+  const {name: {first, last}, picture: {thumbnail}} = userInfo;
+  return `${first} ${last}`;
+}
+
 function App() {
+
+  const [nextPageNumber, setNextPageNumber] = useState(1);
+  const [userInfo, setUserInfo] = useState([]);
+  const [randomUserDataJSON, setRandomUserDataJSON] = useState('');
+
+  const fetchNextUser = () => {
+    fetchRandomData(nextPageNumber).then((randomData) => {
+    // setRandomUserDataJSON(JSON.stringify(randomData, null, 2) || 'No user data found.');
+    if (randomData === undefined) return;
+    const newUserInfo = [...userInfo, ...randomData.results]
+    setUserInfo(newUserInfo);
+    setNextPageNumber(randomData.info.page + 1);
+    })
+}
+
+  useEffect(() => {
+      fetchNextUser(nextPageNumber);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happens!</h2>
+      <button onClick={() => {fetchNextUser()}}>Fetch Next User</button>
+      {
+        userInfo.map((userInfo, idx) => (
+          <div key={idx}>
+          <p>{getFullUserName(userInfo)}</p>
+          <img src={userInfo.picture.thumbnail} alt="" />
+          </div>
+        ))
+      }
+      <p> {randomUserDataJSON} </p>
     </div>
   );
 }
